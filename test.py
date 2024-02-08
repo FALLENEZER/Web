aaab21dc66b3f3f21859d02807e44365cbe183be
FALLENEZER <igumnov.danechka@mail.ru>
2024-02-09 00:13:06 +0300

    Html works

--- test.py
+++
@@ -1,8 +0,0 @@
-string = list(input())
-length = len(string)
-while string != sorted(string):
-    for i in range(length - 1):
-        if string[i] > string[i + 1]:
-            string[i + 1], string[i] = string[i], string[i + 1]
-print(''.join(string))
-    
